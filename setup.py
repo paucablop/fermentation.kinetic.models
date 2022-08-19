5c9c5b74cffe50021b7c9c4bb790b94270f9b6ef
Pau Cabaneros <34301441+paucablop@users.noreply.github.com>
2022-08-19 09:43:32 +0200

    Target Python >=3.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.10",
+    python_requires=">=3.7",
     install_requires=[
         "numpy",
         "pytest",
