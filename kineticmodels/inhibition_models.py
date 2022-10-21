ba53f39747ca62c5d21954f359272c92e069c416
paucablop <pau.cabaneros@gmail.com>
2022-10-21 18:41:10 +0200

    add inhibition models

--- kineticmodels/inhibition_models.py
+++ kineticmodels/inhibition_models.py
@@ -1,78 +1,79 @@
-class Monod:
-    def __init__(self, max_uptake_rate: float, affinity_constant: float) -> None:
-        self.max_uptake_rate = max_uptake_rate
-        self.affinity_constant = affinity_constant
-
-    def calculate_rate(self, substrate_concentration: float) -> float:
-        return (
-            self.max_uptake_rate
-            * substrate_concentration
-            / (self.affinity_constant + substrate_concentration)
-        )
+from dataclasses import dataclass
+from typing import Annotated
+import numpy as np
 
 
-class MonodSubstrateInhibition:
-    def __init__(
-        self,
-        max_uptake_rate: float,
-        affinity_constant: float,
-        inhibition_constant: float,
-    ) -> None:
-        self.max_uptake_rate = max_uptake_rate
-        self.affinity_constant = affinity_constant
-        self.inhibition_constant = inhibition_constant
+@dataclass
+class LinearInhibition:
+    """
+    Expression for the linear inhibition.
+    """
 
-    def calculate_rate(self, substrate_concentration: float) -> float:
-        return (
-            self.max_uptake_rate
-            * substrate_concentration
-            / (
-                self.affinity_constant
-                + substrate_concentration
-                + (substrate_concentration**2) / self.inhibition_constant
-            )
-        )
+    inhibition_constant: Annotated[float, "1 / gram / litre"]
+
+    @classmethod
+    def fromdict(cls, d: dict) -> "LinearInhibition":
+        return cls(**d)
+
+    def rate(self, inhibitor_concentration: Annotated[float, "gram / litre"]) -> float:
+        return 1 - self.inhibition_constant * inhibitor_concentration
+
+    def __parameters__(self) -> dict:
+        return self.__annotations__
 
 
-class MonodSubstrateCompetitiveInhibition:
-    def __init__(
-        self,
-        max_uptake_rate: float,
-        affinity_constant: float,
-        inhibition_constant: float,
-    ) -> None:
-        self.max_uptake_rate = max_uptake_rate
-        self.affinity_constant = affinity_constant
-        self.inhibition_constant = inhibition_constant
+@dataclass
+class ExponentialInhibition:
+    """
+    Expression for the exponential inhibition.
+    """
 
-    def calculate_rate(
-        self,
-        substrate_concentration: float,
-    ) -> float:
-        return self.max_uptake_rate / (
-            (1.0 + self.affinity_constant / substrate_concentration)
-            * (1.0 + substrate_concentration / self.inhibition_constant)
-        )
+    inhibition_constant: Annotated[float, "1 / gram / litre"]
+
+    @classmethod
+    def fromdict(cls, d: dict) -> "ExponentialInhibition":
+        return cls(**d)
+
+    def rate(self, inhibitor_concentration: Annotated[float, "gram / litre"]) -> float:
+        return np.exp(-self.inhibition_constant * inhibitor_concentration)
+
+    def __parameters__(self) -> dict:
+        return self.__annotations__
 
 
-class MonodSubstrateNonCompetitiveInhibition:
-    def __init__(
-        self,
-        max_uptake_rate: float,
-        affinity_constant: float,
-        inhibition_constant: float,
-    ) -> None:
-        self.max_uptake_rate = max_uptake_rate
-        self.affinity_constant = affinity_constant
-        self.inhibition_constant = inhibition_constant
 
-    def calculate_rate(self, substrate_concentration: float) -> float:
-        return (
-            self.max_uptake_rate
-            * substrate_concentration
-            / (
-                self.affinity_constant
-                * (1.0 + substrate_concentration / self.inhibition_constant)
-                + substrate_concentration
-            )
-        )
+@dataclass
+class SuddenInhibition:
+    """
+    Expression for the sudden inhibition.
+    """
+
+    inhibition_constant: Annotated[float, "gram / litre"]
+
+    @classmethod
+    def fromdict(cls, d: dict) -> "SuddenInhibition":
+        return cls(**d)
+
+    def rate(self, inhibitor_concentration: Annotated[float, "gram / litre"]) -> float:
+        return 1 - (self.inhibition_constant / inhibitor_concentration)
+
+    def __parameters__(self) -> dict:
+        return self.__annotations__
+
+@dataclass
+class InverseInhibition:
+    """
+    Expression for the inverse inhibition.
+    """
+
+    inhibition_constant: Annotated[float, "gram / litre"]
+
+    @classmethod
+    def fromdict(cls, d: dict) -> "InverseInhibition":
+        return cls(**d)
+
+    def rate(self, inhibitor_concentration: Annotated[float, "gram / litre"]) -> float:
+        return 1 / (1 + self.inhibition_constant / inhibitor_concentration)
+
+    def __parameters__(self) -> dict:
+        return self.__annotations__
